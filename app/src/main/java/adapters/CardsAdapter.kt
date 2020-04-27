47bcd1bceddfb8a082c121efeb5359de492aa95c
Sam Davey <samkdavey@gmail.com>
2020-04-27 16:44:48 +0100

    - Swipe cards functionality added

---
+++ app/src/main/java/adapters/CardsAdapter.kt
@@ -0,0 +1,33 @@
+package adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
+import com.example.wander.R
+import util.User
+
+class CardsAdapter(context: Context?, resourceId: Int, users: List<User>): ArrayAdapter<User>(context!!, resourceId, users) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var user = getItem(position)
+        var finalView = convertView ?: LayoutInflater.from(context).inflate(R.layout.item, parent, false)
+
+        var name = finalView.findViewById<TextView>(R.id.nameTV)
+        var image = finalView.findViewById<ImageView>(R.id.photoIV)
+
+
+        //Check the null check for the imageUrl
+        name.text = "$(user.name), $(user.age)"
+        Glide.with(context)
+            .load(user?.imageUrl)
+            .into(image)
+
+        return finalView
+    }
+
+}
--- app/src/main/java/fragments/SwipeFragment.kt
+++ app/src/main/java/fragments/SwipeFragment.kt
@@ -1,13 +1,22 @@
 package fragments
 
+import adapters.CardsAdapter
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 
 import com.example.wander.R
 import com.example.wander.WanderCallback
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
+import com.lorentzos.flingswipe.SwipeFlingAdapterView
+import kotlinx.android.synthetic.main.fragment_swipe.*
+import util.*
 
 /**
  * A simple [Fragment] subclass.
@@ -15,9 +24,20 @@ import com.example.wander.WanderCallback
 class SwipeFragment : Fragment() {
 
     private var callback: WanderCallback? = null
+    private lateinit var userId : String
+    private lateinit var userDatabase : DatabaseReference
+    //Puts the items in the array into a view
+    private var cardsAdapter : ArrayAdapter<User>? = null
+    //Array of items
+    private var rowItems = ArrayList<User>()
+    //Variable to reference against when filtering by gender type
+    private var preferredGender : String? = null
+
 
     fun setCallback(callback: WanderCallback){
         this.callback = callback
+        userId = callback.onGetUserId()
+        userDatabase = callback.getUserDatabase()
     }
 
     override fun onCreateView(
@@ -28,4 +48,82 @@ class SwipeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_swipe, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        super.onViewCreated(view, savedInstanceState)
+
+        userDatabase.child(userId).addListenerForSingleValueEvent(object : ValueEventListener{
+            override fun onCancelled(p0: DatabaseError) {
+            }
+
+            override fun onDataChange(p0: DataSnapshot) {
+                val user = p0.getValue(User::class.java)
+                preferredGender = user?.preferredGender
+                populateItems()
+            }
+        })
+
+        cardsAdapter = CardsAdapter(context, R.layout.item, rowItems)
+
+        frame.adapter = cardsAdapter
+        frame.setFlingListener(object : SwipeFlingAdapterView.onFlingListener{
+            override fun removeFirstObjectInAdapter() {
+
+            }
+
+            override fun onLeftCardExit(p0: Any?) {
+
+            }
+
+            override fun onRightCardExit(p0: Any?) {
+
+            }
+
+            override fun onAdapterAboutToEmpty(p0: Int) {
+
+            }
+
+            override fun onScroll(p0: Float) {
+
+            }
+        })
+
+    }
+
+    fun populateItems(){
+        noUsers.visibility = View.GONE
+        progressLayout.visibility = View.VISIBLE
+
+        //This variable gets all the data required for gender preference and orders it accordingly
+        val cardsQuery = userDatabase.orderByChild(DATA_GENDER).equalTo(preferredGender)
+        cardsQuery.addListenerForSingleValueEvent(object: ValueEventListener{
+            override fun onCancelled(p0: DatabaseError) {
+            }
+
+            override fun onDataChange(p0: DataSnapshot) {
+                //If we have already seen this user we don't need to show it
+                p0.children.forEach{child ->
+                    val user = child.getValue(User::class.java)
+                    if(user != null){
+                        var showUser = true
+                        //If this user already has me in their swipe left, don't display
+                        if(child.child(DATA_SWIPES_LEFT).hasChild(userId) && child.child(
+                                DATA_SWIPES_RIGHT).hasChild(userId) && child.child(DATA_MATCHES).hasChild(userId) ){
+                            showUser = false
+                        }
+                        if(showUser){
+                            //Let the adapter know we have updated it
+                            rowItems.add(user)
+                            cardsAdapter?.notifyDataSetChanged()
+                        }
+                    }
+                }
+                progressLayout.visibility = View.GONE
+                //If the row items is empty display the "No users available layout"
+                if(rowItems.isEmpty()){
+                    noUsers.visibility = View.VISIBLE
+                }
+            }
+        })
+    }
 }
